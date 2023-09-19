740d83fe5a52c35de9ec5d4d6627dfa0a168d3f2
asmobina <asmobina10@gmail.com>
2023-09-19 17:42:11 +0330

    add student score and Make some changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
-import Counter from "./Counter"
+import Counter from "./Component/Counter/index"
+import SixteenBox from "./Component/SixteenBox"
+import SlideShow from "./Component/SlideShow"
+import StudentScore from "./Component/StudentScore"
+
 export default function Home() {
   return (
     <>
-      <h1>First session</h1>
-      <Counter></Counter>
+      <h1>Session 001</h1>
+      <Counter />
+
+      <hr></hr>
+      <h1>Session 002</h1>
+      <StudentScore />
+      <hr></hr>
+      
     </>
   )
 }
